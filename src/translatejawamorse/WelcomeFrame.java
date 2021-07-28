021d6fde6dd588961606d97d90c0c25ee70ce57c
Iqblyh <USER@Dape-PC>
2021-07-28 23:15:16 +0800

    Revert ""
    This reverts commit 62f014b91eb17e451e59829aa0e47721b611c476.

--- src/translatejawamorse/WelcomeFrame.java
+++ src/translatejawamorse/WelcomeFrame.java
@@ -55,10 +55,8 @@ public class WelcomeFrame extends javax.swing.JFrame {
 
         welcome.setFont(new java.awt.Font("Verdana", 1, 36)); // NOI18N
         welcome.setForeground(new java.awt.Color(255, 255, 255));
-        welcome.setText("WELCOME");
-        getContentPane().add(welcome, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 195, 210, 40));
-        welcome.getAccessibleContext().setAccessibleName("WELCOME");
-        welcome.getAccessibleContext().setAccessibleDescription("");
+        welcome.setText("Welcome ...");
+        getContentPane().add(welcome, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 210, -1, -1));
 
         arrow.setBackground(new java.awt.Color(255, 255, 255));
         arrow.setText("Mulai");
